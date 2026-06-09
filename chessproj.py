a7e66b475598f24d76c7e6cc91642aabb528bff2
conor-foy <77405566+conor-foy@users.noreply.github.com>
2026-06-09 18:26:10 +0100

    Update chessproj.py

--- chessproj.py
+++ chessproj.py
@@ -116,7 +116,4 @@ class GameState:
 if __name__ == "__main__":
 
     game_state = GameState()
-    game_state.validKnightMoves()
-    game_state.validMoves()
-    print(moves)
     game_state.printBoard()
